8861db551f1dd8ca3d9b70dbe7949245bf5915d7
squeaky1273 <pjriddell22@gmail.com>
2019-09-19 16:41:22 -0700

    added starting code/comments to def attack

--- superheroes.py
+++ superheroes.py
@@ -11,8 +11,12 @@ class Ability:
         self.max_damage = attack_strength
 
     def attack(self):
-
+      ''' Return a value between 0 and the value set by self.max_damage.'''
+      # TODO: Use random.randint(a, b) to select a random attack value.
+      # Return an attack value between 0 and the full attack.
+      # Hint: The constructor initializes the maximum attack value.
         return random.randint(2, 7)
+        pass
 
 if __name__ == "__main__":
     # If you run this file from the terminal
