97b874dc34e38730a2f5761de7ae0afd767a85fe
squeaky1273 <pjriddell22@gmail.com>
2019-09-19 16:33:57 -0700

    added Ability class

---
+++ superheroes.py
@@ -0,0 +1,9 @@
+class Ability:
+    def __init__(self, name, attack_strength):
+        """
+        name: String
+        max_damage: Integer
+
+        """
+        self.name = name
+        self.max_damage = attacks_strength
