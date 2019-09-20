dfb15d4f16ac4f820ad5326a1c7e3f032be5df43
squeaky1273 <pjriddell22@gmail.com>
2019-09-19 17:10:42 -0700

    added Armor class

--- superheroes.py
+++ superheroes.py
@@ -12,15 +12,17 @@ class Ability:
 
     def attack(self):
       ''' Return a value between 0 and the value set by self.max_damage.'''
-      # TODO: Use random.randint(a, b) to select a random attack value.
-      # Return an attack value between 0 and the full attack.
-      # Hint: The constructor initializes the maximum attack value.
-        return random.randint(2, 7)
-        pass
+      return random.randint(0, self.max_damage)
+
+class Armor:
+    def __init__(self, name, max_block):
+        """
+        name: String
+        max_block: integer
+        """
 
 if __name__ == "__main__":
-    # If you run this file from the terminal
-    # this block is executed.
+    # Test the Ability class
     ability = Ability("Debugging Ability", 20)
     print(ability.name)
     print(ability.attack())
