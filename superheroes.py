ad268443e5863462cd0fdfb88654a5bbb05f21d7
squeaky1273 <pjriddell22@gmail.com>
2019-09-19 17:17:22 -0700

    added Hero class

--- superheroes.py
+++ superheroes.py
@@ -27,6 +27,17 @@ class Armor:
         ''' Return a value between 0 and the value set by self.max_damage.'''
         return random.randint(0, self.max_block)
 
+class Hero:
+    def __init__(self, name, starting_health=100):
+        """
+        abilities: List
+        armors: List
+        name: String
+        starting_health: Integer
+        current_health: Integer
+        """
+        
+
 if __name__ == "__main__":
     # Test the Ability class
     ability = Ability("Debugging Ability", 20)
