e82d6a7dd350f21b40ee0821f105ec5ef8d9b38c
squeaky1273 <pjriddell22@gmail.com>
2019-09-19 17:14:25 -0700

    added def block

--- superheroes.py
+++ superheroes.py
@@ -20,6 +20,12 @@ class Armor:
         name: String
         max_block: integer
         """
+        self.name: name
+        self.max_block: max_block
+
+    def block(self):
+        ''' Return a value between 0 and the value set by self.max_damage.'''
+        return random.randint(0, self.max_block)
 
 if __name__ == "__main__":
     # Test the Ability class
