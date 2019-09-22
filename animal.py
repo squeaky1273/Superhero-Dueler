9c48aec475986f122300a9c394727eb567e144eb
squeaky1273 <pjriddell22@gmail.com>
2019-09-21 19:53:46 -0700

    created animal class

---
+++ animal.py
@@ -0,0 +1,9 @@
+class Animal:
+  def __init__(self, name):
+    self.name = name
+
+  def eat(self):
+    print('{} is eating'.format(self.name))
+
+  def drink(self):
+    print('{} is drinking'.format(self.name))
